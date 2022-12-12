859f30de16dfafba00b0d1dfff677d4211fe0788
Anh Cao <ctpanh03@gmail.com>
2022-12-13 02:08:41 +0700

    add img database

--- layouts/script.php
+++ layouts/script.php
@@ -5,4 +5,3 @@
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
-<script src="js/dashboard.js"></script>
